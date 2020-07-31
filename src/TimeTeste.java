c1c792d793e65d0d75782f001788e143a0c795a2
fernandostenico <fer.elias1@gmail.com>
2020-07-31 16:53:37 -0300

    Versao1

---
+++ src/TimeTeste.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TimeTeste {
+    public static void main(String args[]){
+    Scanner teclado = new Scanner(System.in);
+    
+    int h, m, s;
+    
+    h = new hora;
+    set.h
+    
+    }
+}
